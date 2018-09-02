4c09ff3ea092edc68a93e9fbed5336681d6787a8
Beck Larson - Work <becklarson-Work@107-144-156-227.biz.bhn.net>
2018-09-02 00:42:51 -0400

    Login is now completed and rest API works

--- Back End/Project2/src/main/java/com/revatue/Project2/web/LoginCtrl.java
+++ Back End/Project2/src/main/java/com/revatue/Project2/web/LoginCtrl.java
@@ -1,16 +1,19 @@
 package com.revatue.Project2.web;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revatue.Project2.beans.User;
 import com.revatue.Project2.service.UserService;
 
-@Controller
+@RestController
 public class LoginCtrl {
 	
 	@Autowired
@@ -24,5 +27,18 @@ public class LoginCtrl {
 		Optional<User> u = uServ.getUser(id);
 		System.out.println(u);
 		return u;
+	}
+	
+	@PostMapping("/login/user")
+	public Optional<User> testLogin(@RequestBody Map<String, String> body){
+		System.out.println("testing login..." + body);
+		String username = body.get("username");
+		String password = body.get("password");
+		System.out.println(username);
+		System.out.println(password);
+		Optional<User> x = uServ.testUser(username, password);
+		System.out.println(x);
+		return x;
+		
 	}
 }
