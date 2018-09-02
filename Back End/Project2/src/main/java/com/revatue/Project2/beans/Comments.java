c059e9351731900b7646ce19acf0e64558409070
Beck Larson - Work <becklarson-Work@107-144-156-227.biz.bhn.net>
2018-09-02 15:57:42 -0400

    adding create user and update user and admin can promote users

---
+++ Back End/Project2/src/main/java/com/revatue/Project2/beans/Comments.java
@@ -0,0 +1,108 @@
+package com.revatue.Project2.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COMMENTS")
+public class Comments {
+
+	@Id
+	//@SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ", allocationSize = 1)
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "COMMENT_ID")
+	private int cId;
+	
+	@Column(name = "U_ID")
+	private int uId;
+	
+	@Column(name = "TIMESUBMISSION")
+	private String time;
+	
+	@Column(name = "PAGE_ID")
+	private int pageId;
+	
+	@Column(name = "CONTENT")
+	private String content;
+	
+	@Column(name = "COMMENT_STATUS_ID")
+	private int commentStatus;
+
+	public Comments() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Comments(int cId, int uId, String time, int pageId, String content, int commentStatus) {
+		super();
+		this.cId = cId;
+		this.uId = uId;
+		this.time = time;
+		this.pageId = pageId;
+		this.content = content;
+		this.commentStatus = commentStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Comments [cId=" + cId + ", uId=" + uId + ", time=" + time + ", pageId=" + pageId + ", content="
+				+ content + ", commentStatus=" + commentStatus + "]";
+	}
+
+	public int getcId() {
+		return cId;
+	}
+
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+
+	public int getuId() {
+		return uId;
+	}
+
+	public void setuId(int uId) {
+		this.uId = uId;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(int pageId) {
+		this.pageId = pageId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getCommentStatus() {
+		return commentStatus;
+	}
+
+	public void setCommentStatus(int commentStatus) {
+		this.commentStatus = commentStatus;
+	}
+	
+	
+	
+}
