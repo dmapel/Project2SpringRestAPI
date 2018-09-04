13d0ab81b764f88a182d5676e9f112d6b1eb2224
Beck Larson - Work <becklarson-Work@USF-Gold-Wifi-226-125-152.laptops.usf.edu>
2018-09-04 17:04:24 -0400

    adding one to many method

--- Back End/Project2/src/main/java/com/revatue/Project2/repo/CommentRepo.java
+++ Back End/Project2/src/main/java/com/revatue/Project2/repo/CommentRepo.java
@@ -1,9 +1,13 @@
 package com.revatue.Project2.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.revatue.Project2.beans.Comments;
 
 public interface CommentRepo extends CrudRepository<Comments, Integer>{
+	
+	 Comments findByPageId(int pageId);
 
 }
