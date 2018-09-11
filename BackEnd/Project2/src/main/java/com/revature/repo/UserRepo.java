648c328d6b3bb30b8dcf0d2047dd8b17bf130900
Beck Larson - Work <becklarson-Work@USF-Gold-Wifi-226-125-152.laptops.usf.edu>
2018-09-11 13:19:07 -0400

    Login controller now always returns a ResponseEntity.

--- BackEnd/Project2/src/main/java/com/revature/repo/UserRepo.java
+++ BackEnd/Project2/src/main/java/com/revature/repo/UserRepo.java
@@ -14,6 +14,6 @@ public interface UserRepo extends CrudRepository<User, Integer>{
 	
 	Optional<User> findByUsernameAndPassword(String username, String password);
 
-	
+	User findByUId(int id);
 
 }
--- BackEnd/Project2/src/main/java/com/revature/web/PageCtrl.java
+++ BackEnd/Project2/src/main/java/com/revature/web/PageCtrl.java
@@ -1,9 +1,12 @@
 package com.revature.web;
 
-import org.springframework.beans.BeanUtils;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +23,7 @@ public class PageCtrl {
 	
 	@PostMapping("/create/page")
 	public ResponseEntity<Page> createPage(@RequestBody Page body) {
-
+        System.out.println("Incoming is " + body);
 		body =  pServ.createPage(body);
 		return new ResponseEntity<Page>(body, HttpStatus.CREATED);
 	}
@@ -32,6 +35,14 @@ public class PageCtrl {
 
 		
 		return p;
+	}
+	
+	@GetMapping("page/search/{title}")
+	public Page findPage(@PathVariable String title) {
+		System.out.println("Searching for title results...." + title);
+		
+		return pServ.findPage(title);
+		
 	}
 
 }
